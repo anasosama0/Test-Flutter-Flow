a5b70e847d51c3b8aa66d9131a677cdcf71b0673
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-02 12:14:27 +0000

    make the initial page more menu

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -36,12 +36,12 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
       initialLocation: '/',
       debugLogDiagnostics: true,
       refreshListenable: appStateNotifier,
-      errorBuilder: (context, state) => HomePageWidget(),
+      errorBuilder: (context, state) => MoreMenuWidget(),
       routes: [
         FFRoute(
           name: '_initialize',
           path: '/',
-          builder: (context, _) => HomePageWidget(),
+          builder: (context, _) => MoreMenuWidget(),
         ),
         FFRoute(
           name: 'HomePage',
